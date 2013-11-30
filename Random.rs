e6f6965b20b749c94fb8494f9e826779f97c15e8
Liam Kostan <wbkostan@gmail.com>
2013-11-30 14:37:11 -0500

    Untested function to get entropy from Windows OS

--- Random.rs
+++ Random.rs
@@ -1,24 +1,41 @@
+#[cfg(target_os = "win32")]
+use std::libc::{c_void, c_ulong, size_t, malloc, free};
+
 static RAND_MAX:uint = 4294967295; //max unsigned 32 bit integer
 
 #[cfg(target_os = "win32")]
+#[link_name = "advapi32"]
+extern "stdcall" {
+	fn SystemFunction036(buffer: *c_void, size: c_ulong) -> bool;
+}
+
+#[cfg(target_os = "win32")]
 fn getOSEntropy() -> Option<uint>{
-	let mut retVal = None;
-	retVal = Some(5);
-	return retVal;
+	unsafe {
+		let mut ptr: *c_void = malloc(32 as size_t) as *c_void;
+		let result = SystemFunction036(ptr, 32 as c_ulong);
+		if(!result){
+			free(ptr);
+			return None;
+		}
+		else{
+			let package = Some((*ptr as uint));
+			free(ptr);
+			return package;
+		}
+	}
 }
 
 #[cfg(target_os = "linux")]
 #[cfg(target_os = "macos")]
 fn getOSEntropy() -> Option<uint>{
-	let mut retVal = None;
-	retVal = Some(3);
+	let retVal = Some(3);
 	return retVal;
 }
 
 /*Returns a truly random integer value between 0 and RAND_MAX*/
 pub fn rand() -> Option<uint>{
-	let mut retVal = None;
-	retVal = getOSEntropy();
+	let retVal = getOSEntropy();
 	return retVal;
 }
 
