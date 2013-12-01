8ec480ee2d0e704d1cec5f1d022c2f96f24d4211
Liam Kostan <wbkostan@gmail.com>
2013-11-30 21:34:25 -0500

    Fixed linker errors on Windows entropy collection

--- Random.rs
+++ Random.rs
@@ -1,26 +1,37 @@
 #[cfg(target_os = "win32")]
 use std::libc::{c_void, c_ulong, size_t, malloc, free};
 
-static RAND_MAX:uint = 4294967295; //max unsigned 32 bit integer
-
 #[cfg(target_os = "win32")]
-#[link_name = "advapi32"]
+use std::ptr::null;
+
+#[cfg(target_os = "win32", target_arch = "x86")]
+#[link_name = "crypt32"]
 extern "stdcall" {
-	fn SystemFunction036(buffer: *c_void, size: c_ulong) -> bool;
+	fn CryptAcquireContextW(x: *c_void, c: *c_void, p: *c_void, t: c_ulong, f: c_ulong) -> bool;
+	fn CryptReleaseContext(x: *c_void, f: c_ulong) -> bool;
+	fn CryptGenRandom(x: *c_void, l: c_ulong, d: *c_void) -> bool;
 }
 
 #[cfg(target_os = "win32")]
+#[fixed_stack_segment]
 fn getOSEntropy() -> Option<uint>{
 	unsafe {
-		let mut ptr: *c_void = malloc(32 as size_t) as *c_void;
-		let result = SystemFunction036(ptr, 32 as c_ulong);
-		if(!result){
+		let context : *c_void = null();
+		let n : *c_void = null();
+		let res1 = CryptAcquireContextW(context, n, n, 22 as c_ulong, 0);
+		if(!res1){return None;}
+		
+		let ptr: *c_void = malloc(32 as size_t) as *c_void;
+		let res2 = CryptGenRandom(context, 32 as c_ulong, ptr);
+		if(!res2){
 			free(ptr);
+			CryptReleaseContext(context, 0);
 			return None;
 		}
 		else{
-			let package = Some((*ptr as uint));
+			let package = Some((ptr as uint));
 			free(ptr);
+			CryptReleaseContext(context, 0);
 			return package;
 		}
 	}
