376fb11e0c9aaf6bb33b0642dfd1e317b0f0a663
wbkostan <wbkostan@gmail.com>
2013-12-01 18:03:17 -0500

    Added structure for weak rand functionality on mac/linux

--- Random.rs
+++ Random.rs
@@ -7,7 +7,7 @@ use std::ptr::null;
 #[cfg(target_os = "win32", target_arch = "x86")]
 #[link_name = "crypt32"]
 extern "stdcall" {
-	fn CryptAcquireContextW(x: *c_void, c: *c_void, p: *c_void, t: c_ulong, f: c_ulong) -> bool;
+	fn CryptAcquireContextA(x: *c_void, c: *c_void, p: *c_void, t: c_ulong, f: c_ulong) -> bool;
 	fn CryptReleaseContext(x: *c_void, f: c_ulong) -> bool;
 	fn CryptGenRandom(x: *c_void, l: c_ulong, d: *c_void) -> bool;
 }
@@ -20,13 +20,13 @@ extern "stdcall" {
 
 #[cfg(target_os = "win32")]
 #[fixed_stack_segment]
-fn getOSEntropy() -> Option<uint>{
+fn getOSEntropy() -> uint{
 	unsafe {
 		let context : *c_void = malloc(8 as size_t) as *c_void; //size of context
 		let n : *c_void = null();
-		let res1 = CryptAcquireContextW(context, n, n, 1 as c_ulong, 0);
+		let res1 = CryptAcquireContextA(context, n, n, 1 as c_ulong, 4026531848 as c_ulong);
 		if(!res1){
-			let error = Some(GetLastError() as uint);
+			let error = (GetLastError() as uint);
 			println("Context bailed");
 			return error;
 		}
@@ -34,7 +34,7 @@ fn getOSEntropy() -> Option<uint>{
 		let ptr: *c_void = malloc(8 as size_t) as *c_void;
 		let res2 = CryptGenRandom(context, 8 as c_ulong, ptr);
 		if(!res2){
-			let error = Some(GetLastError() as uint);
+			let error = (GetLastError() as uint);
 			free(ptr);
 			CryptReleaseContext(context, 0);
 			free(context);
@@ -42,7 +42,7 @@ fn getOSEntropy() -> Option<uint>{
 			return error;
 		}
 		else{
-			let package = Some((ptr as uint));
+			let package = (ptr as uint);
 			free(ptr);
 			CryptReleaseContext(context, 0);
 			free(context);
@@ -51,14 +51,32 @@ fn getOSEntropy() -> Option<uint>{
 	}
 }
 
+#[cfg(target_os = "linux")]
+#[cfg(target_os = "macos")]
+pub fn getOSEntropy() -> uint{
+	let mut mem : ~[u8] = ~[1,1,1,1];
+	
+	/* Read from file to mem */
+	
+	let mut res : u32 = 0;
+	for val in mem.iter(){
+		res = res << 8;
+		res = res | (*val as u32);
+	}
+	
+	return (res as uint);
+}
 
 /*Returns a truly random integer value between 0 and RAND_MAX*/
-pub fn rand() -> Option<uint>{
-	let retVal = getOSEntropy();
-	return retVal;
+pub fn srand() -> Option<uint>{
+	return None;
+}
+
+pub fn wrand() -> uint{
+	return getOSEntropy();
 }
 
 fn main() {
-	let num = rand();
-	println(num.unwrap().to_str());
+	let num = wrand();
+	println(num.to_str());
 }
